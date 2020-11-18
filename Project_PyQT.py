538aa34144f0448a7e655c2a6693a16d066aaa92
Семен <semenbondarenko9@gmail.com>
2020-11-18 13:50:15 +0500

    2

--- Project_PyQT.py
+++ Project_PyQT.py
@@ -4,6 +4,12 @@ import sqlite3
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
+class Window2(QMainWindow):
+    def __init__(self):
+        super(Window2, self).__init__()
+        self.setWindowTitle('Window2')
+        uic.loadUi("svodka_project.ui", self)
+
 
 class Window(QMainWindow):
     def __init__(self):
@@ -15,6 +21,25 @@ class Window(QMainWindow):
     def search_number(self):
         cur = self.con.cursor()
         query = f"SELECT * FROM Base_cars WHERE number = '{self.number.text()}' and reg = '{self.reg.text()}'"
+        res = cur.execute(query).fetchall()
+        if len(res) > 1:
+            self.connect(self.show_window_2)
+            self.name.setText(res[0])
+            self.vin.setText(res[1])
+            self.power.setText(res[2])
+            self.workV.setText(res[3])
+            self.year.setText(res[4])
+            self.color.setText(res[5])
+            self.people.setText(res[6])
+            self.weight.setText(res[7])
+            self.dtp.setText(res[8])
+            self.taxi.setText(res[9])
+        else:
+            self.output.setText("Некорректный ввод")
+
+    def search_vin_number(self):
+        cur = self.con.cursor()
+        query = f"SELECT * FROM Base_cars WHERE VIN_number = '{self.vin_number.text()}'"
         print(query)
         res = cur.execute(query).fetchall()
         print(res)
