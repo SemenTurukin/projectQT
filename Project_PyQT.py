b0872ed9347441317c9b0396cf9c837a8499abba
core2q6600 <saladay@yandex.ru>
2020-11-22 23:52:42 +0500

    Добавлен вывод ошибок

--- Project_PyQT.py
+++ Project_PyQT.py
@@ -49,8 +49,13 @@ class Window(QMainWindow):
         res = cur.execute(query).fetchall()
         print(res)
 
-if __name__ == "__main__":
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    wnd = Window()
-    wnd.show()
-    sys.exit(app.exec_())
+    form = Window()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
